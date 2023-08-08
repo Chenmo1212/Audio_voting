ae8e22434a54e70c4f55adf72a6acf4ecb6c1056
chenmo <chenmo1212@qq.com>
2023-08-08 15:07:55 +0100

    update user info

--- src/resources/data/userInfo.js
+++ src/resources/data/userInfo.js
@@ -4,127 +4,127 @@ const userInfo = [
     "id": "001",
     "desc": "秋天",
     "url": "https://cdn.chenmo1212.cn/files/audio/刘芮汐.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/1.png"
-  },
-  {
-    "name": "刘轩辰",
-    "id": "002",
-    "desc": "奶奶的熊二",
-    "url": "https://cdn.chenmo1212.cn/files/audio/刘轩辰.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/2.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/刘芮汐.jpg"
   },
   {
     "name": "叶芸睿",
     "id": "003",
     "desc": "沁园春·雪",
     "url": "https://cdn.chenmo1212.cn/files/audio/叶芸睿.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/3.png"
-  },
-  {
-    "name": "夏自恒",
-    "id": "004",
-    "desc": "没办法就是长得帅",
-    "url": "https://cdn.chenmo1212.cn/files/audio/夏自恒.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/4.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/叶芸睿.jpg"
   },
   {
     "name": "康嘉馨",
     "id": "005",
     "desc": "先叫声前辈",
     "url": "https://cdn.chenmo1212.cn/files/audio/康嘉馨.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/5.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/康嘉馨.jpg"
   },
   {
     "name": "张子瀚",
     "id": "006",
     "desc": "沁园春·雪",
     "url": "https://cdn.chenmo1212.cn/files/audio/张子瀚.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/6.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/张子瀚.jpg"
   },
   {
     "name": "杨君瑞",
     "id": "007",
     "desc": "其实我知道",
     "url": "https://cdn.chenmo1212.cn/files/audio/杨君瑞.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/7.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/杨君瑞.jpg"
   },
   {
     "name": "殷紫涵",
     "id": "008",
     "desc": "满江红",
     "url": "https://cdn.chenmo1212.cn/files/audio/殷紫涵.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/8.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/殷紫涵.jpg"
   },
   {
     "name": "秦王",
     "id": "009",
     "desc": "好饿的毛毛虫",
     "url": "https://cdn.chenmo1212.cn/files/audio/秦王.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/9.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/秦王.jpg"
   },
   {
     "name": "聂杨恩",
     "id": "010",
     "desc": "风味漫威",
     "url": "https://cdn.chenmo1212.cn/files/audio/聂杨恩.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/8.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/聂杨恩.jpg"
   },
   {
     "name": "聂杨晴",
     "id": "011",
     "desc": "再别康桥",
     "url": "https://cdn.chenmo1212.cn/files/audio/聂杨晴.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/1.png"
-  },
-  {
-    "name": "苏锦程",
-    "id": "012",
-    "desc": "听妈妈的话",
-    "url": "https://cdn.chenmo1212.cn/files/audio/苏锦程.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/2.png"
-  },
-  {
-    "name": "苏锦鹏",
-    "id": "013",
-    "desc": "学猫叫",
-    "url": "https://cdn.chenmo1212.cn/files/audio/苏锦鹏.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/3.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/聂杨晴.jpg"
   },
   {
     "name": "萧若妍",
     "id": "014",
     "desc": "影视配音",
     "url": "https://cdn.chenmo1212.cn/files/audio/萧若妍.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/4.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/萧若妍.jpg"
   },
   {
     "name": "贺文若",
     "id": "015",
     "desc": "轻轻",
     "url": "https://cdn.chenmo1212.cn/files/audio/贺文若.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/5.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/贺文若.jpg"
   },
   {
     "name": "陆俞蒙",
     "id": "016",
     "desc": "荷叶圆圆",
     "url": "https://cdn.chenmo1212.cn/files/audio/陆俞蒙.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/6.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/陆俞蒙.jpg"
   },
   {
     "name": "陈柏亦",
     "id": "017",
     "desc": "江南春",
     "url": "https://cdn.chenmo1212.cn/files/audio/陈柏亦.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/7.png"
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/陈柏亦.jpg"
   },
   {
     "name": "陈泳妍",
     "id": "018",
     "desc": "樱桃小丸子",
     "url": "https://cdn.chenmo1212.cn/files/audio/陈泳妍.mp3",
-    "avatar": "https://cdn.chenmo1212.cn/img/8.png"
-  }
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/陈泳妍.jpg"
+  },
+  {
+    "name": "刘轩辰",
+    "id": "002",
+    "desc": "奶奶的熊二",
+    "url": "https://cdn.chenmo1212.cn/files/audio/刘轩辰.mp3",
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/刘轩辰.jpg"
+  },
+  {
+    "name": "夏自恒",
+    "id": "004",
+    "desc": "没办法就是长得帅",
+    "url": "https://cdn.chenmo1212.cn/files/audio/夏自恒.mp3",
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/夏自恒.jpg"
+  },
+  {
+    "name": "苏锦鹏",
+    "id": "013",
+    "desc": "学猫叫",
+    "url": "https://cdn.chenmo1212.cn/files/audio/苏锦鹏.mp3",
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/苏锦鹏.jpg"
+  },
+  {
+    "name": "苏锦程",
+    "id": "012",
+    "desc": "听妈妈的话",
+    "url": "https://cdn.chenmo1212.cn/files/audio/苏锦程.mp3",
+    "avatar": "https://cdn.chenmo1212.cn/files/audio/苏锦程.jpg"
+  },
 ];
 
 export default userInfo;
